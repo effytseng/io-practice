620c45097b87fb59bfa98156c84826cd7ba452c7
effytseng <yy0124125@gmail.com>
2018-12-26 17:17:17 +0800

    練習 java oop

--- src/main/java/com/effy/Player.java
+++ src/main/java/com/effy/Player.java
@@ -2,10 +2,37 @@ package com.effy;
 
 public class Player {
 
+	public static final int magicNum = 10;
 	public String name;
 	public int hp;
 	public int sp;
 	public int attack;
 	public int defense;
+	public int bonus = 0;
 
+	public Player(String nam, int hp, int sp, int attack, int defense) {
+		name = nam;
+		this.hp = hp;
+		this.sp = sp;
+		this.attack = attack;
+		this.defense = defense;
+	}
+
+	public String whoAttack() {
+		return name + "發動攻擊";
+	}
+
+	public void playProcess(Player other) { // 原來可以這樣帶進去
+		if (sp < magicNum) {
+			System.out.println(name + "沒魔了");
+			return;
+		}
+		sp -= magicNum;
+		other.hp -= (attack - other.defense);
+		System.out.println(other.name + "生命:" + other.hp + name + "魔力:" + sp);
+	}
+
+	public boolean isAlive() {
+		return hp > 0;
+	}
 }
--- src/main/java/com/effy/ioPractice.java
+++ src/main/java/com/effy/ioPractice.java
@@ -1,56 +1,32 @@
 package com.effy;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class ioPractice {
 
-	private static Scanner sc = new Scanner(System.in);
+//	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Player playerA = new Player();
-		playerA.name = "玩家A";
-		playerA.hp = 100;
-		playerA.sp = 200;
-		playerA.attack = 80;
-		playerA.defense = 30;
+		Player playerA = new Player("玩家A", 100, 200, 80, 30);
 
-		Player playerB = new Player();
-		playerB.name = "玩家B";
-		playerB.hp = 150;
-		playerB.sp = 200;
-		playerB.attack = 40;
-		playerB.defense = 50;
+		Player playerB = new Player("玩家B", 150, 200, 40, 50);
 
 		int roundNum = 1;
 
-		while (playerA.hp > 0 && playerB.hp > 0) {
+		while (playerA.isAlive() && playerB.isAlive()) {
 			System.out.println("=====遊戲開始=====");
 			System.out.println("===第" + roundNum + "回合===");
 			if (roundNum % 2 == 1) {
-				if (playerA.sp < 10) {
-					System.out.println("a沒魔了");
-					roundNum += 1;
-					continue;
-				}
-				System.out.println("a發動攻擊");
-				playerA.sp -= 10;
-				playerB.hp -= (playerA.attack - playerB.defense);
-				System.out.println("B生命:" + playerB.hp + "A魔力:" + playerA.sp);
+				System.out.println(playerA.whoAttack());
+				playerA.playProcess(playerB);
 			} else {
-				if (playerB.sp < 10) {
-					System.out.println("b沒魔了");
-					roundNum += 1;
-					continue;
-				}
-				System.out.println("B發動攻擊");
-				playerB.sp -= 10;
-				playerA.hp -= (playerB.attack - playerA.defense);
-				System.out.println("A生命:" + playerA.hp + "B魔力:" + playerB.sp);
+				System.out.println(playerB.whoAttack());
+				playerB.playProcess(playerA);
 			}
 			roundNum += 1;
 		}
 		System.out.println("=====遊戲結束=====");
-		if (playerA.hp > 0) {
+		if (playerA.isAlive()) {
 			System.out.println(playerA.name + "獲勝");
 		} else {
 			System.out.println(playerB.name + "獲勝");
